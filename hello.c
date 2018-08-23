1e875dda766988d7fed78bdfd4002c5bfd9d4fda
IIGravityII <42561496+IIGravityII@users.noreply.github.com>
2018-08-23 14:32:53 -0500

    Added my own name and major

--- hello.c
+++ hello.c
@@ -10,7 +10,11 @@
 
 int main(int argc, char **argv) {
 
-  printf("Brady Fangmeyer Computer Engineer\n");
+<<<<<<< HEAD
+  printf("Brady Fangmeyer Computer Engineer; Sean Hollibaugh Mechanical Engineering\n");
+=======
+  printf("Brady Fangmeyer Computer Engineer. Ryan Lampe Computer Engineering\n");
+>>>>>>> c944f0fe753e1d142527512b18f5847312711197
 
   return 0;
 }
