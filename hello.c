c2e9093681ef57e2e1857367a64355d37db29248
b14fangmeyer <42366020+b14fangmeyer@users.noreply.github.com>
2018-08-23 14:06:06 -0500

    Brady Fangmeyer Computer Engineer

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Brady Fangmeyer Kai Guo
+ * Date: 2018/08/21
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Brady Fangmeyer Computer Engineer\n");
+
+  return 0;
+}
